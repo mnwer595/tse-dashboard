0bc3f9ce317e3b99d6c289b5e09db142b7678b37
mnwer595 <64558393+mnwer595@users.noreply.github.com>
2023-02-08 21:46:55 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -106,6 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
