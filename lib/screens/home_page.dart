04e7107b28ad64a94284746692d8ef9a38aa5aad
mnwer595 <64558393+mnwer595@users.noreply.github.com>
2023-02-09 04:03:16 +0300

    ng
    fn

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("TSE Dashboard123"),
+        title: Text("TSE Dashboard3"),
       ),
       body: Column(
         children: [
